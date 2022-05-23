de7a3e89c1f358e619b5825092d9133729c54d82
Shreyam Adhikari <shreyam1008@gmail.com>
2022-05-23 16:29:22 +0545

    scss setup

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// new react version change on render methind to older version.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
