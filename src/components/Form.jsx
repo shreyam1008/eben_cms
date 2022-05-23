3d97ab13909462055e21670f88b35a83b07b0344
Shreyam Adhikari <shreyam1008@gmail.com>
2022-05-23 18:50:15 +0545

    form layout and design

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,25 @@
+import { Button, Input, Paper, TextField, Typography } from "@mui/material";
+import "./Form.scss";
+
 const Form = () => {
   return (
     <div className="Form">
-      <h1>form</h1>
-      Name: <input type="text" /> <br />
-      Title: <input type="text" />
-      <br />
-      Theme: <input type="text" />
-      <br />
-      Bio: <input type="text" />
-      <br />
-      Ideas: <input type="text" />
-      <br />
+      <Paper elevation={3} className="form_fields">
+        <Typography variant="h6">Form</Typography>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+
+        <TextField id="outlined-basic" label="Title" variant="outlined" />
+
+        <TextField id="outlined-basic" label="Theme" variant="outlined" />
+
+        <TextField id="outlined-basic" label="Bio" variant="outlined" />
+
+        <TextField id="outlined-basic" label="Ideas" variant="outlined" />
+        <TextField id="outlined-basic" label="Quotes" variant="outlined" />
+        <Button variant="contained" className="submit_button">
+          Submit
+        </Button>
+      </Paper>
     </div>
   );
 };
